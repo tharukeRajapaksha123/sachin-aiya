d86d6f059acfd8c852579d7dc89a533823011b5b
Tharushi <87642831+IT21048500@users.noreply.github.com>
2023-05-23 07:56:57 +0530

    ADD:nav bar done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GeneralInformationComponent } from './components/general-information/general-information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GeneralInformationComponent
   ],
   imports: [
     BrowserModule
